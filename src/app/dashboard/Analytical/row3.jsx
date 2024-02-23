f5c7be9735649678c4b1d18ab065c85e01484160
marjanmokhtari <mimmokhtarii@gmail.com>
2024-02-23 17:44:10 +0330

    finish customer

--- src/app/dashboard/Analytical/row3.jsx
+++ src/app/dashboard/Analytical/row3.jsx
@@ -2,6 +2,13 @@
 import React from 'react'
 import { BarChart } from '@mui/x-charts/BarChart';
 import { CgMenuGridO } from "react-icons/cg";
+import { BsArrowDownLeftCircleFill } from "react-icons/bs";
+
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import { SparkLineChart } from '@mui/x-charts/SparkLineChart';
+
+
 
 import './mystyle.css'
 const uData = [20, 15, 30, 25, 10, 15];
@@ -18,12 +25,14 @@ export default function Row_three() {
     return (
         <section className=' w-full my-5 '>
             <section className=' w-full ps-2   *:lg:w-1/3 *:w-full flex flex-wrap lg:flex-nowrap   '>
-                <section className='  lg:me-4 my-3 lg:my-0 border '>
+                <section className='  lg:me-4 my-3 lg:my-0 border border-[#343e50] shadow-sm shadow-[#343e50] rounded-md  '>
                     <Employee_Salary></Employee_Salary>
                 </section>
                 <section className=' *:w-full lg:me-4 '>
                     <section className=' flex flex-wrap  md:flex-nowrap *:md:w-1/2 *:w-full mb-2 '>
-                        <section className=' bg-emerald-300 my-3 lg:my-0 md:me-2'>dd</section>
+                        <section className='  my-3 lg:my-0 md:me-2'>
+                            <Customers></Customers>
+                        </section>
                         <section className=' bg-emerald-900 my-3 lg:my-0 md:ms-2'>lorem</section>
                     </section>
                     <section className=' bg-yellow-400 my-3 lg:my-0 '>1</section>
@@ -46,14 +55,26 @@ function Employee_Salary() {
                 <My_chart></My_chart>
             </div>
             <div className=' w-1/2 justify-between flex'>
-                <div>
-                    <CgMenuGridO></CgMenuGridO>
-                    <div className=' *:flex *:w-full flex flex-wrap'>
-                        <span>Salary</span>
+                <div className=' flex items-center'>
+                    <span className=' p-2'>
+                        <CgMenuGridO className=' text-2xl bg-[#253662] text-[#5d87ff] rounded-md '></CgMenuGridO>
+
+                    </span>
+                    <div className=' *:flex *:w-full flex flex-wrap *:text-[#7c8fac]'>
+                        <span className=' text-sm'>Salary</span>
                         <span>$36,358</span>
                     </div>
                 </div>
-                <div></div>
+                <div className=' flex items-center'>
+                    <span className=' p-2'>
+                        <CgMenuGridO className=' text-2xl bg-[#f2f6fa] text-[#7c8fac] rounded-md '></CgMenuGridO>
+
+                    </span>
+                    <div className=' *:flex *:w-full flex flex-wrap *:text-[#7c8fac]'>
+                        <span className=' text-sm'>Profit</span>
+                        <span>$5,296</span>
+                    </div>
+                </div>
             </div>
         </section>
     )
@@ -70,5 +91,52 @@ function My_chart() {
             xAxis={[{ data: xLabels, scaleType: 'band' }]}
             colors={['#557be4']}
         />
+    )
+}
+
+
+function Customers() {
+    return (
+        <section className=' w-full text-[#7c8fac]  *:w-full  border border-[#343e50] shadow-sm shadow-[#343e50] rounded-md '>
+            <div className=' *:w-full flex flex-wrap *:flex p-5 '>
+                <span>Customers</span>
+                <span className=' my-2 text-[#fff] text-xl font-semibold'>36,358</span>
+                <div className=' flex items-center  my-2 '>
+                    <BsArrowDownLeftCircleFill className='rounded-full text-xl text-[#fff] me-2'></BsArrowDownLeftCircleFill>
+                    <span>+9%</span>
+                </div>
+            </div>
+            <div>
+                <Cust_chrt></Cust_chrt>
+            </div>
+        </section>
+    )
+}
+
+function Cust_chrt() {
+    const [showTooltip, setShowTooltip] = React.useState(true);
+    const handleTooltipChange = (event) => {
+      setShowTooltip(event.target.checked);
+    };
+  
+    return (
+        <div className=' w-full '>
+            <Stack direction="column" sx={{ width: '100%' }}>
+
+
+                <Stack direction="row" sx={{ width: '100%' }}>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <SparkLineChart
+                            data={[30, 25, 35, 40, 20, 30,40]}
+                            height={100}
+                            colors={['#44b7f7']}
+                            showTooltip={showTooltip}
+
+                        />
+                    </Box>
+
+                </Stack>
+            </Stack>
+        </div>
     )
 }
