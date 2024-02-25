573b3d26ea293809f592caa04b844dc44bff0da3
marjanmokhtari <mimmokhtarii@gmail.com>
2024-02-25 18:39:24 +0330

    finish row 4

--- src/app/dashboard/Analytical/row4.jsx
+++ src/app/dashboard/Analytical/row4.jsx
@@ -1,4 +1,12 @@
+"use client"
 import React from 'react'
+import Image from 'next/image'
+import S6 from '../../assets/img/s6.jpg'
+import S4 from '../../assets/img/s4.jpg'
+import S7 from '../../assets/img/s7.jpg'
+import S9 from '../../assets/img/s9.jpg'
+
+
 
 export default function Row_four() {
     return (
@@ -97,11 +105,11 @@ function Recent_Transactions() {
                     </div>
                     <div className=' *:text-sm'>
                         <p>Payment Done</p>
-                        
+
                     </div>
                 </div>
-             
-        
+
+
             </section>
         </section>
     )
@@ -110,6 +118,129 @@ function Recent_Transactions() {
 
 function Top_Projects() {
     return (
-        <section className=' w-full'></section>
+        <section className=' w-full border border-[#343e50] shadow-sm shadow-[#343e50] rounded-md'>
+            <section className=' w-full p-5 *:w-full'>
+                <section className=' flex justify-between'>
+                    <section>
+                        <h6 className=' text-[#eaeff4]'>Top Projects</h6>
+                        <span className=' text-sm text-[#7c8fac]'>Best Products</span>
+                    </section>
+        
+                </section>
+
+
+                <section className=''>
+                    <section className=' *:my-4'> 
+                        <section className=' flex   justify-between *:text-sm text-[#eaeff4]'>
+                            <div className=' w-[40%]'>
+                                <span>product</span>
+                            </div>
+                            <div className=' w-[20%]'>
+                                <span>prgrees</span>
+                            </div>
+                            <div className=' w-[20%]'>
+                                <span>status</span>
+                            </div>
+                            <div className=' w-[20%]'>
+                                <span>Sales</span>
+                            </div>
+                        </section>
+
+                        <section className=' flex  justify-between capitalize py-2'>
+                            <div className=' w-[40%]'> 
+                                <div className=' flex items-center'>
+                                    <figure>
+                                        <Image className=' w-[50px] rounded-md ' src={S6} alt="" />
+                                    </figure>
+                                    <div className=' flex flex-wrap *:w-full ps-2'>
+                                        <span className=' text-sm text-[#eaeff4]'>Gaming Console</span>
+                                        <span className=' text-xs text-[#7c8fac]'>Leather Purse</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>78%</span>
+                            </div>
+                             <div  className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>low</span>
+                            </div>
+                            <div  className=' w-[20%] flex items-center text-sm' >
+                                <span className='text-[#7c8fac]'>$3.9k</span>
+                            </div>
+                        </section>
+                        <section className=' flex  justify-between capitalize py-2'>
+                            <div className=' w-[40%]'> 
+                                <div className=' flex items-center'>
+                                    <figure>
+                                        <Image className=' w-[50px] rounded-md ' src={S9} alt="" />
+                                    </figure>
+                                    <div className=' flex flex-wrap *:w-full ps-2'>
+                                        <span className=' text-sm text-[#eaeff4]'>Headphone Boat</span>
+                                        <span className=' text-xs text-[#7c8fac]'>Fashion</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>78%</span>
+                            </div>
+                             <div  className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>medium</span>
+                            </div>
+                            <div  className=' w-[20%] flex items-center text-sm' >
+                                <span className='text-[#7c8fac]'>$3.9k</span>
+                            </div>
+                        </section>
+                        <section className=' flex  justify-between capitalize py-2'>
+                            <div className=' w-[40%]'> 
+                                <div className=' flex items-center'>
+                                    <figure>
+                                        <Image className=' w-[50px] rounded-md ' src={S7} alt="" />
+                                    </figure>
+                                    <div className=' flex flex-wrap *:w-full ps-2'>
+                                        <span className=' text-sm text-[#eaeff4]'>Red Velvate Dress</span>
+                                        <span className=' text-xs text-[#7c8fac]'>Womens Fashion</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>78%</span>
+                            </div>
+                             <div  className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>high</span>
+                            </div>
+                            <div  className=' w-[20%] flex items-center text-sm' >
+                                <span className='text-[#7c8fac]'>$3.9k</span>
+                            </div>
+                        </section>
+                        <section className=' flex  justify-between capitalize py-2'>
+                            <div className=' w-[40%]'> 
+                                <div className=' flex items-center'>
+                                    <figure>
+                                        <Image className=' w-[50px] rounded-md ' src={S4} alt="" />
+                                    </figure>
+                                    <div className=' flex flex-wrap *:w-full ps-2'>
+                                        <span className=' text-sm text-[#eaeff4]'>Leather Purse</span>
+                                        <span className=' text-xs text-[#7c8fac]'>Electronics</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>78%</span>
+                            </div>
+                             <div  className=' w-[20%] flex items-center text-sm'>
+                                <span className='text-[#7c8fac]'>Critical</span>
+                            </div>
+                            <div  className=' w-[20%] flex items-center text-sm' >
+                                <span className='text-[#7c8fac]'>$3.9k</span>
+                            </div>
+                        </section>
+
+                       
+                    </section>
+                </section>
+            </section>
+
+        </section>
     )
 }
+
