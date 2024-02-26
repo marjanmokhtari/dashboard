a83cb44fbae60f5f7daec347b237e9fbac4ecffc
marjanmokhtari <mimmokhtarii@gmail.com>
2024-02-26 13:34:46 +0330

    This is a new commit for what I originally planned to be amended

--- src/app/dashboard/eCommerce/row1.jsx
+++ src/app/dashboard/eCommerce/row1.jsx
@@ -1,6 +1,12 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import Bg2 from '../../assets/img/bg2.webp'
+import Pay from '../../assets/img/paypal.svg'
+import { BsArrowDownLeftCircleFill } from "react-icons/bs";
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import { SparkLineChart } from '@mui/x-charts/SparkLineChart';
 
 export default function row1() {
     return (
@@ -39,9 +45,54 @@ function Mathew() {
 
 function Payments() {
     return (
-        <section className=' w-full flex justify-between flex-wrap md:flex-nowrap *:border-[#343e50] *:shadow-sm *:shadow-[#343e50] *:rounded-md '>
-            <section className=' border w-full md:w-1/2 md:me-3 mb-3 md:mb-0 '>dsa</section>
-            <section className=' border w-full md:w-1/2 md:ms-3 mt-3 md:mt-0 '>lsd</section>
+        <section className=' w-full flex justify-between flex-wrap md:flex-nowrap *:border-[#343e50] text-[#eaeff4] *:shadow-sm *:shadow-[#343e50] *:rounded-md '>
+            <section className=' border w-full md:w-1/2 md:me-3 mb-3 md:mb-0 p-5 *:w-full flex flex-wrap '>
+                <figure className=' py-3'>
+                    <Image src={Pay}></Image>
+                </figure>
+                <span className=' pt-2 text-[#7c8fac]'>Payments</span>
+                <span className=' text-xl'>$678,298</span>
+                <div className=' flex py-3'>
+                    <BsArrowDownLeftCircleFill className=' me-2 text-lg text-[#13deb9]'></BsArrowDownLeftCircleFill>
+                    <span>+9%</span>
+                </div>
+            </section>
+
+
+            <section className=' border w-full md:w-1/2 md:me-3 mb-3 md:mb-0 *:w-full flex flex-wrap '>
+                <section className=' py-3 *:w-full flex flex-wrap p-5'>
+                    <span className=' text-sm text-[#7c8fac] '>Projects</span>
+                    <span className=' text-xl text-[#eaeff4] '>78,298</span>
+                </section>
+
+
+                <section className=' flex py-3'>
+                    <Chart_pay></Chart_pay>
+                </section>
+            </section>
+        </section>
+    )
+}
+
+function Chart_pay() {
+
+    return (
+        <section className=' w-full flex justify-center'>
+            <Stack direction="row" className=' w-full'>
+                <Box sx={{ flexGrow: 1 }}>
+                    <SparkLineChart
+                        data={[30, 25, 35, 20, 30, 40,25]}
+                        xAxis={{
+                            scaleType: 'time',
+                        
+                        }}
+                        height={100}
+                        showTooltip
+                        showHighlight
+                    />
+                </Box>
+                
+            </Stack>
         </section>
     )
 }
